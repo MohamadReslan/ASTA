99747ebc19a17d604d524b2d6f406d5468c96dd0
Maxime Wang <maxime.wang@efrei.net>
2023-11-09 08:43:25 +0100

    Add dependencies to pom.xml to run the project

--- src/main/java/fr/efrei2023/ASTA/controller/ApprenticeController.java
+++ src/main/java/fr/efrei2023/ASTA/controller/ApprenticeController.java
@@ -2,15 +2,12 @@ package fr.efrei2023.ASTA.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.efrei2023.ASTA.model.Model.UserInfoModel;
-import fr.efrei2023.ASTA.model.entity.UserEntity;
 import fr.efrei2023.ASTA.model.sessionbean.UserSessionBean;
 import jakarta.ejb.EJB;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -19,9 +16,8 @@ import java.io.PrintWriter;
 public class ApprenticeController extends HttpServlet {
     @EJB
     private UserSessionBean userSessionBean;
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        HttpSession session = request.getSession();
-        String apprenticeId = (String) session.getAttribute("apprenticeSelected");
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String apprenticeId = (String) request.getSession().getAttribute("apprenticeSelected");
         UserInfoModel apprenticeSelected = userSessionBean.getUserInfo(Integer.parseInt(apprenticeId));
         String userJson = new ObjectMapper().writeValueAsString(apprenticeSelected);
         response.setContentType("application/json");
